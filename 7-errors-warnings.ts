aa1b455b208071a84eafb269f52aea10bb35323f
Colby Garland <colbygarland@gmail.com>
2021-08-18 09:09:37 -0700

    Added more errros/warnings

--- 7-errors-warnings.ts
+++ 7-errors-warnings.ts
@@ -12,3 +12,30 @@ const user: User = {
 // incorrect type errors
 const age: number = '26';
 console.log(age);
+
+// not callable errors
+user();
+
+// non-exception failures
+// in js, this is simply undefined
+user.email;
+
+// typos
+const message = 'Avengers, assemble!';
+message.toLocaleLowercase();
+message.toLocalLowerCase();
+// TODO: write the correct function call here
+
+// uncalled functions
+function flipCoin() {
+  // we meant Math.random()
+  return Math.random < 0.5;
+}
+
+// also catches basic logic errors
+const value = Math.random() < 0.5 ? 'a' : 'b';
+if (value !== 'a') {
+  // do work
+} else if (value === 'b') {
+  // this part is unreachable
+}
